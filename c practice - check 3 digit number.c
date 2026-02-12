79ec3169212dda38b51b2169652bfbb4a6d9061d
dhilakshna-suresh <dhilakshnasuresh@gmail.com>
2026-02-12 10:41:41 +0530

    Add files via upload

---
+++ c practice - check 3 digit number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    n = 345;
+    if(n>100 && n<999)
+    {
+        printf("three digit number");
+    }
+    else
+    {
+        printf("zero digit number");
+    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+    return 0;
+}
