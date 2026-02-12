ecf61bc4d18e47a78b1b66fdf8442769b6aea9dd
dhilakshna-suresh <dhilakshnasuresh@gmail.com>
2026-02-12 09:31:29 +0530

    Add files via upload

---
+++ c practice - check leap year.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int year;
+    scanf("%d",&year);
+    if((year%4 && year!=100)||(year&400))
+    {
+        printf("leap year");
+    }
+    else
+    {
+        printf("not a leap year");
+    }
+    return 0;
+}
