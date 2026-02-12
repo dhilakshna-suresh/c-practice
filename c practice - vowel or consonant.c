87b59aa246c020681f819ea562d800083980adeb
dhilakshna-suresh <dhilakshnasuresh@gmail.com>
2026-02-12 10:04:31 +0530

    Add files via upload

---
+++ c practice - vowel or consonant.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n='a','e','i','o','u')
+    {
+        printf("vowel");
+    }
+    else
+    {
+        printf("consonant");
+    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+    return 0;
+}
