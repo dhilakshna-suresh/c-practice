f4aba7d9a8150b02819bde9cbebe6a78d312e36f
dhilakshna-suresh <dhilakshnasuresh@gmail.com>
2026-02-12 10:27:33 +0530

    Add files via upload

---
+++ c practice - voting eligible .c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int age;
+    age = 18;
+    if(age>=18)
+    if(num%3==0 && num%7==0);
+    {
+        printf("eligible to vote");
+    }
+    else
+    {
+        printf("not eligible to vote");
+    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+    return 0;
+}
