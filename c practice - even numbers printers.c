c10d3bf363a30f719be84194dead936a6933c350
dhilakshna-suresh <dhilakshnasuresh@gmail.com>
2026-02-17 09:00:26 +0530

    Add files via upload

---
+++ c practice - even numbers printers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{   
+    int N;
+    scanf("%d",&N);
+    for(int i=2;i<=N;i+=2){
+        printf("%d",i);
+        if(i+2<=N){
+            printf(" ");
+        }
+    }
+    return 0;
+}
