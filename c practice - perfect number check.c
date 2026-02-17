74da250153adca1a3e76ea43c5189b30570df2a1
dhilakshna-suresh <dhilakshnasuresh@gmail.com>
2026-02-17 12:03:35 +0530

    Add files via upload

---
+++ c practice - perfect number check.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int N,i,sum=0;
+    scanf("%d",&N);
+    for(i=1;i<N;i++)
+    {
+        if(N%i==0){
+            sum=sum+i;
+        }
+    }
+    if(sum==N)
+    printf("yes");
+    else
+    printf("no");
+    return 0;
+} 
