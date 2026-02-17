eb8b93df36ddbd7c52bf30722bde9b62782e3616
dhilakshna-suresh <dhilakshnasuresh@gmail.com>
2026-02-17 12:05:14 +0530

    Add files via upload

---
+++ c practice - strong number check.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n,temp,d,sum=0,fact,i;
+    scanf("%d",&n);
+    temp=n;
+    while(temp>0){
+        d=temp%10;
+        fact=1;
+        for(i=1;i<=d;i++)
+        fact=fact*i;
+        sum=sum+fact;
+        temp=temp/10;
+    }
+    if(sum==n)
+    printf("yes");
+    else
+    printf("no");
+    return 0;
+} 
