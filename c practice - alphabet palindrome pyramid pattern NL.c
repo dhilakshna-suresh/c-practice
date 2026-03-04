f1fa032155b51f2522d6ff3b1c20488ebb2f35df
dhilakshna-suresh <dhilakshnasuresh@gmail.com>
2026-03-04 11:27:20 +0530

    Add files via upload

---
+++ c practice - alphabet palindrome pyramid pattern NL.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j;
+    for(int i=1;i<=4;i++){
+        
+        for(int j=1;j<=4-i;j++){
+         printf(" ");
+            
+        }
+         
+         for(int j=1;j<=i;j++){
+         printf("%c",'A'+j-1);
+         }
+         
+         for(int j=i-1;j>=1;j--){
+         printf("%c",'A'+j-1);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
