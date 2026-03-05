74deb474a13775ef9240b1a3cff048f2aba2eb69
dhilakshna-suresh <dhilakshnasuresh@gmail.com>
2026-03-05 10:02:21 +0530

    Add files via upload

---
+++ C Practice-Verify sorted daily sales report .c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n-1;i++)
+    {
+        if(arr[i]>arr[i+1])
+        {
+            printf("no");
+            return 0;
+        }
+    }
+     printf("yes");
+    
+
+    return 0;
+}
