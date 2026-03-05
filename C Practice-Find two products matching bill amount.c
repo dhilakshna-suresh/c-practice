52b8a398e8f4a497c3debce0b788b45079a93d4f
dhilakshna-suresh <dhilakshnasuresh@gmail.com>
2026-03-05 11:19:42 +0530

    Add files via upload

---
+++ C Practice-Find two products matching bill amount.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,a[100],sum,i,j,found=0;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+
+    scanf("%d",&sum);
+
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]+a[j]==sum)
+            {
+                printf("%d %d",a[i],a[j]);
+                found=1;
+                break;
+            }
+        }
+        if(found)
+            break;
+    }
+
+    if(found==0)
+        printf("No pair");
+
+    return 0;
+}
