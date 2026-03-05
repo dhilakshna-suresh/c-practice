2733cc5fa0bda6ca600317debcaa2081dd910b41
dhilakshna-suresh <dhilakshnasuresh@gmail.com>
2026-03-05 10:01:44 +0530

    Add files via upload

---
+++ C Practice-Count students scoring a specific mark.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int x;
+    int count=0;
+    
+    scanf("%d",&n);
+    
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&x);
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==x)
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+
+    return 0;
+}
