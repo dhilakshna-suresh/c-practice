5c7df44345a8cd4c92614ac5eefeb36c299e38eb
dhilakshna-suresh <dhilakshnasuresh@gmail.com>
2026-03-05 09:44:32 +0530

    Add files via upload

---
+++ c practice-odd number star pyramid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,s;
+    for(int i=1;i<=5;i++)
+    {
+        for(s=1;s<=5-i;s++)
+        printf(" ");
+        
+         for(int j=1;j<=2*i-1;j++)
+         {
+         printf("*");
+         }
+        printf("\n");
+    }
+
+    return 0;
+}
