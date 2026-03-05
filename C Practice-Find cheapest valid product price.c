86799e6356faeaf767615c98cf2e0dcfb3b05e8a
dhilakshna-suresh <dhilakshnasuresh@gmail.com>
2026-03-05 11:22:52 +0530

    Add files via upload

---
+++ C Practice-Find cheapest valid product price.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,a[100],i,count=0;
+    float sum=0,avg;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        sum=sum+a[i];
+    }
+
+    avg=sum/n;
+
+    for(i=0;i<n;i++)
+        if(a[i]>avg)
+            count++;
+
+    printf("%d",count);
+
+    return 0;
+}
