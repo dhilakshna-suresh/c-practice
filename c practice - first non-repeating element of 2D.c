0b982b5872bcf3ab559b6305878f12d96203dc6b
dhilakshna-suresh <dhilakshnasuresh@gmail.com>
2026-03-11 10:06:32 +0530

    Add files via upload

---
+++ c practice - first non-repeating element of 2D.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int r,c,i,j,k,l,a[50][50],count;
+
+    scanf("%d%d",&r,&c);
+
+    for(i=0;i<r;i++)
+    for(j=0;j<c;j++)
+    scanf("%d",&a[i][j]);
+
+    for(i=0;i<r;i++)
+     for(j=0;j<c;j++)
+     {
+      count=0;
+
+        for(k=0;k<r;k++)
+         for(l=0;l<c;l++)
+            if(a[i][j]==a[k][l])
+             count++;
+
+            if(count==1)
+            {
+             printf("%d",a[i][j]);
+             return 0;
+            }
+        }
+}
