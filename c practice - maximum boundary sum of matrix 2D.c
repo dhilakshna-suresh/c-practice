8361e4e204d646e686ccec27f7bd977e5c0a7736
dhilakshna-suresh <dhilakshnasuresh@gmail.com>
2026-03-11 10:11:17 +0530

    Add files via upload

---
+++ c practice - maximum boundary sum of matrix 2D.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int r,c,i,j,a[50][50],sum=0;
+
+    scanf("%d%d",&r,&c);
+
+    for(i=0;i<r;i++)
+    for(j=0;j<c;j++)
+    scanf("%d",&a[i][j]);
+
+    for(i=0;i<r;i++)
+        for(j=0;j<c;j++)
+        {
+         if(i==0 || i==r-1 || j==0 || j==c-1)
+            sum=sum+a[i][j];
+        }
+
+    printf("%d",sum);
+    return 0;
+}
