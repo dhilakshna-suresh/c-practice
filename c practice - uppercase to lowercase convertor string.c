c7cc357bd0174017035a271d248f81c8626e2218
dhilakshna-suresh <dhilakshnasuresh@gmail.com>
2026-03-17 09:05:49 +0530

    Add files via upload

---
+++ c practice - uppercase to lowercase convertor string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i;
+
+    scanf("%[^\n]", str);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = str[i] - 32;
+        }
+    }
+
+    printf("%s", str);
+
+    return 0;
+}
